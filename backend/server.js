079ea32d2f546a61537a53a4391acc956f8c3361
developerchetram <patelchetram49@gmail.com>
2025-10-07 12:13:41 +0530

    add: mongo db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import {app} from "./src/app.js";
+import {connectDB} from "./src/db/db.js";
 
+//db connection
+connectDB();
+
+//server
 const PORT = process.env.PORT || 3001;
 app.listen(process.env.PORT, () => {  
 console.log(`Server is running on port  http://localhost:${PORT}`);
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import { indexRouter } from "../src/routes/index.routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+
 export const app = express();
 //middlewares
 app.use(express.json());
